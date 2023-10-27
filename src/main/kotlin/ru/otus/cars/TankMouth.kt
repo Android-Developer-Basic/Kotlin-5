00f5a54ebacc5ec59e78bf5b5625627097b00ae5
Aleh Barushka <1.alehbarushka@gmail.com>
2023-10-27 17:25:47 +0300

    feat: add HW #4

---
+++ src/main/kotlin/ru/otus/cars/TankMouth.kt
@@ -0,0 +1,22 @@
+package ru.otus.cars
+
+sealed class TankMouth {
+    open fun open() {}
+    open fun close() {}
+}
+
+open class PetrolMouth : TankMouth() {
+    override fun open() = println("Горловина бензинового бака открыта")
+
+    override fun close() = println("Горловина бензинового бака закрыта")
+
+    open fun fuelPetrol(liters: Int) {}
+}
+
+open class LpgMouth : TankMouth() {
+    override fun open() = println("Горловина газового бака открыта")
+
+    override fun close() = println("Горловина газового бака закрыта")
+
+    open fun fuelLpg(liters: Int) {}
+}
